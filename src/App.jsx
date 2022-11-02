5bb108d2177504b1268ceda702a3186f3e7bd0cd
ZeekBeakCreek <89820141+ZeekBeakCreek@users.noreply.github.com>
2022-11-02 13:52:06 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [jokes, setJokes] = useState([]);
+  // useState is set to epmty array to initialize state
   const [randomNumber, setRandomNumber] = useState(0);
+  // useState is set to 0 as typeOf randomNumber will be a number
 
   useEffect(() => {
     fetch(`https://api.chucknorris.io/jokes/random`)
       .then((res) => res.json())
       .then((data) => setJokes(data));
   }, [randomNumber]);
+  // randomNumber put into dependency array so when called will rerender and provinde a new joke
 
   return (
     <div className="main-container">
